c80d27dd14ce1189d048fc20d1ee99d09d542baa
Ashley Burton <s168972@ucs.ac.uk>
2016-11-05 15:02:15 +0000

    Changed kraken swipe

--- Unity Project/Assets/Scripts/Kraken.js
+++ Unity Project/Assets/Scripts/Kraken.js
@@ -5,6 +5,7 @@ public var touchEnd: Vector2;
 public var touchDistanceX: float;
 public var anim: Animator;
 public var canKill: boolean;
+public var valid: boolean;
 
 function Start () 
 {
@@ -15,31 +16,45 @@ function Start ()
  
 function Update()
 {
+	//Gets touch input. If touch starts near tenticle and ends next to it and is killable then kill it
 	if (Input.touchCount > 0)
 	var touch: Touch = Input.GetTouch(0);
 	
 	switch(touch.phase)
 	{
 	
-	case TouchPhase.Began: if(touch.position.x > 100 && touch.position.x < 200)
+	case TouchPhase.Began: if(touch.position.x > 400 && touch.position.x < 500)
 							{
-								if(touch.position.y > 100 && touch.position.y < 200)
+								if(touch.position.y > 160 && touch.position.y < 230)
 								{
 									touchStart = touch.position;
+									valid = true;
 								}
+								else
+								{
+									valid = false;
+								}
+							}
+							else 
+							{
+								valid = false;
 							}	
 							break;
 	case TouchPhase.Ended:  touchEnd = touch.position;
 							touchDistanceX = touchStart.x - touchEnd.x;
-							if (touchDistanceX < - 100)
+							if (touchDistanceX < - 100 && valid)
 							{
-								Debug.Log("Swipe right");
-								Destroy(this.gameObject);
+								Debug.Log("Kraken Hit");
+								if(canKill)
+								{
+									Destroy(this.gameObject);
+								}
 							}
 							break;
 	}
 } 
 
+//Loops through animation of up and down
 function Down () 
 {
 	anim.SetBool("Down", true);
@@ -55,10 +70,10 @@ function Up ()
 	anim.SetBool("Up", true);
 	canKill = true;
 	yield WaitForSeconds(5);
-	UpHold();
+	Hold();
 }
 
-function UpHold() 
+function Hold() 
 {
 	Down();
 }
