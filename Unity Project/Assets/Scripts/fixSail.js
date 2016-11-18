657892f42e4996d7849979910209be6198ce7d36
DaltonKirk <daltonkirk95@yahoo.co.uk>
2016-11-18 19:29:34 +0000

    sail breaks, but you cant fix it yet....

---
+++ Unity Project/Assets/Scripts/fixSail.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+public var mousePos: Vector3;
+public var bonePos: GameObject;
+public var sail: GameObject;
+public var distance: float;
+public var pos: Vector3;
+public var boatCam: Camera;
+
+function OnMouseDrag()
+{
+	mousePos.x = (Input.mousePosition.x);
+	mousePos.y = (Input.mousePosition.y);
+	mousePos.z = (distance);
+	pos = boatCam.ScreenToWorldPoint (mousePos);
+	bonePos.transform.position = pos;
+	sail.SetActive (true);
+}
