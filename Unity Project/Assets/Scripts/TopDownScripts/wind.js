cda7f367c6f2a2027d0e729b65b26ed4be7d05c6
DaltonKirk <daltonkirk95@yahoo.co.uk>
2016-12-02 12:33:26 +0000

    trying to make wind particle effect

---
+++ Unity Project/Assets/Scripts/TopDownScripts/wind.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+public var boat: Rigidbody;
+public var force: Vector3;
+
+function Start () {
+
+}
+
+function Update () 
+{
+if (Input.GetKeyDown(KeyCode.W))
+	{
+		boat.AddForce(force);
+	}
+
+}
