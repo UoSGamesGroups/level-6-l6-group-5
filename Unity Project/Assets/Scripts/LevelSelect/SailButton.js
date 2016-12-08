3518fe8b46cd94cf87c292d1d7805f2de0b08375
Ashley Burton <s168972@ucs.ac.uk>
2016-12-08 23:59:17 +0000

    Adjusted upgrade UI and made other UI buttons bigger

--- Unity Project/Assets/Scripts/LevelSelect/SailButton.js
+++ Unity Project/Assets/Scripts/LevelSelect/SailButton.js
@@ -1,11 +1,13 @@
 ﻿#pragma strict
 public var sailsPanel: GameObject;
 public var boatToShowSails: GameObject;
+public var zoneText: GameObject;
 public var panelOpen: boolean;
 
 function SailsButton ()
 {
     panelOpen = !panelOpen;
-    sailsPanel.SetActive (panelOpen);
-    boatToShowSails.SetActive (panelOpen);
+    sailsPanel.SetActive(panelOpen);
+    boatToShowSails.SetActive(panelOpen);
+    zoneText.SetActive(!panelOpen);
 }
