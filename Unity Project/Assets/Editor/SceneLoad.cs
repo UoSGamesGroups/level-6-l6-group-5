1bea25128334186afb42b0392dcf4bc06da44309
Ashley Burton <s168972@ucs.ac.uk>
2016-12-16 14:23:58 +0000

    Made changes to Skybox and Water

---
+++ Unity Project/Assets/Editor/SceneLoad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class NewEditorTest {
+
+	[MenuItem("Open Scene/Boss")]
+	public static void OpenBoss() {
+		
+		OpenScene("Boss");
+	}
+
+	[MenuItem("Open Scene/Level Select")]
+	public static void OpenLevelSelect() {
+
+		OpenScene("LevelSelect");
+	}
+
+	[MenuItem("Open Scene/Exploration")]
+	public static void OpenExploration() {
+
+		OpenScene("scene 1");
+	}
+	public static void OpenScene(string name)
+	{
+		if(EditorApplication.SaveCurrentSceneIfUserWantsTo())
+		{
+			EditorApplication.OpenScene("Assets/Scenes/" + name + ".unity");
+		}
+	}
+}
