d109406509adc2cd149c6ffbc81e930e639f6c06
DaltonKirk <daltonkirk95@yahoo.co.uk>
2017-01-26 17:52:33 +0000

    added camera modes and editor script

---
+++ Unity Project/Assets/Scripts/TopDownScripts/inspectorButton.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+@CustomEditor (topDownCameraScript)
+public class RunMethod extends Editor
+{
+    function OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        var myScript : topDownCameraScript = target;
+        if(GUILayout.Button("Change Camera Mode"))
+        {
+            myScript.ChangeCameraMode();
+        }
+    }
+}
---
+++ Unity Project/Assets/Scripts/TopDownScripts/topDownCameraScript.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+public var staticPos: Vector3;
+public var chaseCameraOn: boolean;
+public var maxPosX: float;
+public var minPosX: float;
+public var maxPosZ: float;
+public var minPosZ: float;
+public var player: GameObject;
+
+
+
+function Start () 
+{
+	staticPos = transform.position;
+}
+
+function Update () 
+{
+	
+	
+	
+	if (chaseCameraOn)
+	{
+		transform.position.x = player.transform.position.x;
+		transform.position.z = player.transform.position.z;
+		
+	if(transform.position.x > maxPosX)
+		transform.position.x = maxPosX;
+	
+	if(transform.position.x < minPosX)
+		transform.position.x = minPosX;
+	
+	if(transform.position.z > maxPosZ)
+		transform.position.z = maxPosZ;
+	
+	if(transform.position.z < minPosZ)
+		transform.position.z = minPosZ;
+	}
+	
+	
+}
+
+function ChangeCameraMode()
+{
+	chaseCameraOn = !chaseCameraOn;
+	
+	if (!chaseCameraOn)
+		transform.position = staticPos;
+}
