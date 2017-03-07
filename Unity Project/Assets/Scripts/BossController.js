36010abf7127fe22fa2a1b1b35bca33272f0cf4f
Ashley Burton <s168972@ucs.ac.uk>
2017-03-07 18:08:36 +0000

    Small changes to bosses

--- Unity Project/Assets/Scripts/BossController.js
+++ Unity Project/Assets/Scripts/BossController.js
@@ -21,23 +21,23 @@ function Awake ()
 				boss = Instantiate(big, bossLocation, transform.rotation);
 				boss.GetComponent(Boss).nextShotTime.x = 3.5 - (currentLevel / 10);
 				boss.GetComponent(Boss).nextShotTime.y = 4 - (currentLevel / 10);
-				boss.GetComponent(Boss).baseDamage = 12;
+				boss.GetComponent(Boss).baseDamage = 15;
 				boss.GetComponent(Boss).healthStart = currentLevel * 100;
 				boss.GetComponent(Boss).moveSpeed = 0;
 				break;
 		case 1:
 				boss = Instantiate(medium, bossLocation, transform.rotation);
-				boss.GetComponent(Boss).nextShotTime.x = 2.5 - (currentLevel / 10);
-				boss.GetComponent(Boss).nextShotTime.y = 3.5 - (currentLevel / 10);
-				boss.GetComponent(Boss).baseDamage = 10;
+				boss.GetComponent(Boss).nextShotTime.x = 2 - (currentLevel / 10);
+				boss.GetComponent(Boss).nextShotTime.y = 2.5 - (currentLevel / 10);
+				boss.GetComponent(Boss).baseDamage = 7;
 				boss.GetComponent(Boss).healthStart = currentLevel * 70;
 				boss.GetComponent(Boss).moveSpeed =  0;
 				break;
 		case 2:
 				boss = Instantiate(small, bossLocation, transform.rotation);
-				boss.GetComponent(Boss).nextShotTime.x = 2 - (currentLevel / 10);
-				boss.GetComponent(Boss).nextShotTime.y = 2.5 - (currentLevel / 10);
-				boss.GetComponent(Boss).baseDamage = 7;
+				boss.GetComponent(Boss).nextShotTime.x = 2.5 - (currentLevel / 10);
+				boss.GetComponent(Boss).nextShotTime.y = 3 - (currentLevel / 10);
+				boss.GetComponent(Boss).baseDamage = 10;
 				boss.GetComponent(Boss).healthStart = currentLevel * 50;
 				boss.GetComponent(Boss).moveSpeed = currentLevel * 0.5;
 				break;
